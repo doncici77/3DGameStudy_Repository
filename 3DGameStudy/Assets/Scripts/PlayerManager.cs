d39f4f1fc84448e1aec8e0e07419b92abdbbaa76
doncici77 <pkh2956@naver.com>
2025-03-06 13:24:26 +0900

    플레이어메니저 스크립트 변수 선언

--- 3DGameStudy/Assets/Scripts/PlayerManager.cs
+++ 3DGameStudy/Assets/Scripts/PlayerManager.cs
@@ -1,61 +1,24 @@
 using UnityEngine; // NameSpace : 소속
 
-class Item
-{
-    public virtual void Eat()
-    {
-        Debug.Log("그냥 먹다");
-    }
-}
-
-class Potion : Item
-{
-    public override void Eat()
-    {
-        Debug.Log("포션을 먹다");
-    }
-
-    public void Heal()
-    {
-        Debug.Log("힐!");
-    }
-}
-
-class Cookie : Item
-{
-
-}
-
-
 public class PlayerManager : MonoBehaviour
 {
-    int[] num = new int[100];
-    int k = 0;
-    Potion potion = new Potion();
-    Item item = new Item();
-    Item potion2 = new Potion();
-    Cookie cookie = new Cookie();
+    public float moveSpeed = 5.0f; // 플레이어 이동 속도
+    public float mouseSensitivity = 100.0f; // 마우스 감도
+    public Transform cameraTransform; // 카메라의 Transform
+    public CharacterController characterController;
+    public Transform playerHead; // 플레이어 머리위치 (1인칭 모드를 위해서)
+    public float thirdPersonDistance = 3.0f; // 3인칭 모드에서 플레이어와 카메라의 거리
+    public Vector3 thirdPersonOffset = new Vector3(0, 1.5f, 0f); // 3인칭 모드에서 카메라 오프셋
+    public Transform playerLookObj; // 플레이어 시양 위치
+
+    public float zoomDistance = 1.0f; // 카메라가 확대될때의 거리(3인칭 모드에서 사용)
+    public float zoomSpeed = 5.0f; // 확대축소가 되는 속도
+    public float defaultFov = 60.0f; // 기본 카메라 시야각
+    public float zoomFov = 30.0f; // 확대 시 카메라 시야각(1인칭 모드에서 사용)
 
     void Start()
     {
-        // for문 예제
-        for (int i = 2; i < 10; i++)
-        {
-            for(int j = 1; j < 10; j++)
-            {
-                num[k] = i * j; // 저장
-                Debug.Log($"{i} X {j} = {num[k]}"); // 출력
-                k ++;
-            }
-        }
 
-        // 오버라이드 예제
-        item.Eat();
-        potion.Eat();
-        potion2.Eat();
-        cookie.Eat();
-
-        potion.Heal();
     }
 
     void Update()
